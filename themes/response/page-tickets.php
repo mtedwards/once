9c95bcfb71b2bcb4284d2051b80f8dee7b7fcc08
Matt Edwards <matt@emptyhead.com.au>
2014-03-14 09:22:04 +1100

    Extra content area added to Tickets page

--- themes/response/page-tickets.php
+++ themes/response/page-tickets.php
@@ -22,8 +22,15 @@
   			  	<div class="small-12 columns orangegrad">
 	  			  	<div class="row">
 	  			  		<div class="small-12 medium-8 medium-centered large-6 large-centered columns">
+		  			  		
 		  			  		<h2>Book Tickets Online<br><span>Via <?php the_field('ticket_company'); ?></span></h2>
-		  			  		<a href="<?php the_field('tickets_url') ?>" class="black-button button expand" onClick="_gaq.push(['_trackEvent', 'Outbound', 'Purchase', 'tickets <?php the_title() ?>']);">Book Now</a>
+		  			  		
+		  			  		<a href="<?php the_field('tickets_url') ?>" class="black-button button expand" 
+		  			  		
+		  			  		onClick="_gaq.push(['_trackEvent', 'Outbound', 'Purchase', 'tickets <?php the_title() ?>']);">
+		  			  			Book Now
+		  			  		</a>
+		  			  		
 	  			  		</div>
 	  			  	</div>
   			  	</div>
@@ -33,15 +40,28 @@
   			 	<div class="small-12 medium-4 columns">
   			 		<div class="nicebox">
 	  			 		<h2><?php the_field('groups_heading'); ?></h2>
+	  			 		
 	  			 		<h2><span><?php the_field('group_message'); ?></span></h2>
-	  			 		<a href="mailto:<?php the_field('group_email'); ?>" class="yellow-button button expand" onClick="_gaq.push(['_trackEvent', 'Outbound', 'Purchase', 'groups <?php the_title() ?>']);" target="_blank">make enquiry</a>
+	  			 		
+	  			 		<a href="mailto:<?php the_field('group_email'); ?>" class="yellow-button button expand" 
+	  			 		onClick="_gaq.push(['_trackEvent', 'Outbound', 'Purchase', 'groups <?php the_title() ?>']);" target="_blank">
+	  			 			make enquiry	  			 		
+	  			 		</a>
   			 		</div>
   			 	</div>
   			 	
   			 	<div class="small-12 medium-4 columns">
   			 		<div class="nicebox">
 	  			 		<h2><?php the_field('dates'); ?></h2>
-	  			 		<h2><span><?php the_field('theatre'); ?><br><a href="<?php the_field('address'); ?>" target="_blank"><?php the_field('google_map_link'); ?></a></span></h2>
+	  			 		<h2>
+	  			 			<span>
+		  			 			<?php the_field('theatre'); ?>
+		  			 			<br>
+		  			 			<a href="<?php the_field('google_map_link'); ?>" target="_blank">
+		  			 				<?php the_field('address'); ?>
+		  			 			</a>
+	  			 			</span>
+	  			 		</h2>
 		  			 	<h3>
 			  			 	<?php the_field('performance_schedule'); ?>
 		  			 	</h3>
@@ -50,20 +70,40 @@
   			 	
   			 	<div class="small-12 medium-4 columns">
   			 		<div class="nicebox">
-		  			 	<h2><?php the_field('special_heading'); ?><br><span><?php the_field('special_details'); ?></span></h2>
-		  			 	<img src="<?php the_field('special_image'); ?>" alt="<?php the_field('special_heading'); ?>">
-		  			 	<a href="<?php the_field('special_link') ?>" class="yellow-button button expand" onClick="_gaq.push(['_trackEvent', 'Outbound', 'Purchase', 'tickets <?php the_title() ?>']);"><?php the_field('special_button') ?></a>
+		  			 	<h2>
+		  			 		<?php the_field('special_heading'); ?>
+		  			 		<br>
+		  			 		<span>
+		  			 			<?php the_field('special_details'); ?>
+		  			 		</span>
+		  			 	</h2>
+		  			 	
+		  			 	<a href="<?php the_field('special_link') ?>" 
+		  			 	onClick="_gaq.push(['_trackEvent', 'Outbound', 'Purchase', 'tickets <?php the_title() ?>']);">
+		  			 		<img src="<?php the_field('special_image'); ?>" alt="<?php the_field('special_heading'); ?>">
+		  			 	</a>
+		  			 	
+		  			 	<a href="<?php the_field('special_link') ?>" class="yellow-button button expand" 
+		  			 	onClick="_gaq.push(['_trackEvent', 'Outbound', 'Purchase', 'tickets <?php the_title() ?>']);">
+		  			 		<?php the_field('special_button') ?>
+		  			 	
+		  			 	</a>
 		  			 </div>
   			 	</div>
   			 </div>                        
- 
+  			 <div class="row">
+  			 	<div class="small-12 columns">
+	  			 	<?php the_field('footer_note'); ?>
+  			 	</div>
+  			 </div>
                 <?php
                   $sponsors = get_field('sponsors'); 
                   if($sponsors) { ?>
                    <div class="small-12 columns center">
                     <br>
                     <?php foreach($sponsors as $sponsor) { ?>
-                    	<a href="<?php echo $sponsor['sponsor_link'] ?>" target="_blank" onClick="_gaq.push(['_trackEvent', 'Outbound', 'Sponsor', '<?php echo $sponsor['title'] ?>']);">
+                    	<a href="<?php echo $sponsor['sponsor_link'] ?>" target="_blank" 
+                    	onClick="_gaq.push(['_trackEvent', 'Outbound', 'Sponsor', '<?php echo $sponsor['title'] ?>']);">
 	                    	<img src="<?php echo $sponsor['sponsor_image']; ?>">
                     	</a>
                     <?php } // end foreach ?>
