f6bafec382dd14ea35c799bfc083d385b9af033d
Matt Edwards <matt@emptyhead.com.au>
2014-04-08 16:48:16 +1000

    Add Facebook Tracking to Header

--- themes/response/header.php
+++ themes/response/header.php
@@ -20,17 +20,7 @@
 	
 	<link href="https://plus.google.com/113907490633717096036" rel="publisher" />
 	
-	<script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-   
-    ga('create', 'UA-44051658-1', 'oncemusical.com.au');
-    ga('send', 'pageview');   
-  </script>
-  
-  <script src="https://apis.google.com/js/plusone.js"></script>
+	<?php include_once("tracking-header.php") ?>
 
 <?php wp_head(); ?>
 
---
+++ themes/response/tracking-header.php
@@ -0,0 +1,30 @@
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+   
+    ga('create', 'UA-44051658-1', 'oncemusical.com.au');
+    ga('send', 'pageview');   
+  </script>
+  
+  <script src="https://apis.google.com/js/plusone.js"></script>
+  
+  <!-- Facebook Conversion Code for Once Website Visit -->
+  <script type="text/javascript">
+    var fb_param = {};
+    fb_param.pixel_id = '6015503595886';
+    fb_param.value = '0.00';
+    fb_param.currency = 'AUD';
+    (function(){
+    var fpw = document.createElement('script');
+    fpw.async = true;
+    fpw.src = '//connect.facebook.net/en_US/fp.js';
+    var ref = document.getElementsByTagName('script')[0];
+    ref.parentNode.insertBefore(fpw, ref);
+    })();
+  </script>
+  <noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/offsite_event.php?id=6015503595886&amp;value=0&amp;currency=AUD" /></noscript>
+  
+  
+  
